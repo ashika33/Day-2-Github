61273783d133e125aeb58d53915e5765b80b293e
AdityaMitti <45398989+AdityaMitti@users.noreply.github.com>
2018-11-29 12:30:41 -0500

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Day2{
 	public static void main (String[]args){
 
@@ -6,7 +7,18 @@ public class Day2{
 		System.out.println("Aditya");
 		System.out.println("Blue");
 		System.out.println("Airbus");
+	public static ArrayList<Integer> convertToArrayList (int number){
+		ArrayList <Integer> list = new ArrayList<>();
+				while (number!=0){
+					list.add(0, number%10);
+					number/=10;
 
+				}
+		return list;
+	}
+
+	public static void main (String[]args){
+		System.out.println(convertToArrayList(8950));
 
 
 
