e42fb7b775b8737189dc3e2fa0aadecf96b15e32
ashika33 <45399045+ashika33@users.noreply.github.com>
2018-11-29 12:21:56 -0500

    method 1
    new change

--- Day2.java
+++ Day2.java
@@ -1,11 +1,19 @@
+import java.util.ArrayList;
 public class Day2{
+	public static ArrayList<Integer> convertToArrayList (int number){
+		ArrayList <Integer> list = new ArrayList<>();
+				while (number!=0){
+					list.add(0, number%10);
+					number/=10;
+
+				}
+		return list;
+	}
+
 	public static void main (String[]args){
 
 
-		System.out.println("Ashika");
-		System.out.println("Aditya");
-		System.out.println("Blue");
-
+		System.out.println(convertToArrayList(8950));
 
 
 
